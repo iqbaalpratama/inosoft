81682d05720541cf2ed1242c4a98d3272f9915ed
iqbaalpratama <iqbaalpratama23@gmail.com>
2022-08-14 19:18:00 +0700

    add table cars and motorcycle

--- database/migrations/2022_08_14_101557_create_cars_table.php
+++ database/migrations/2022_08_14_101557_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('year');
+            $table->string('color');
+            $table->integer('price');
+            $table->string('machine');
+            $table->string('type');
+            $table->string('passenger_total');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('cars');
     }
 }
--- database/migrations/2022_08_14_101744_create_motorcycle_table.php
+++ database/migrations/2022_08_14_101744_create_motorcycle_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMotorcycleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('motorcycle', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('year');
+            $table->string('color');
+            $table->integer('price');
+            $table->string('machine');
+            $table->string('suspension_type');
+            $table->string('transmission_type');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('motorcycle');
     }
 }
